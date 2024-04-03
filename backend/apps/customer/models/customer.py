f274bd29826a215b7a6264d02b39a65c9b6af35d
Sam1AT <alileon32@gmail.com>
2024-04-03 20:14:35 +0330

    add customer base model(without migrations)

---
+++ backend/apps/customer/models/customer.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=11, unique=True)
+    national_code = models.CharField(max_length=10, unique=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name + " | " + self.national_code
